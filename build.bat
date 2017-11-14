618168f312b9829fbf63cadf2c870d8b11c8d844
Zhao <zsxfordream@163.com>
2017-11-14 23:41:26 +0800

    add build.bat

---
+++ build.bat
@@ -0,0 +1,23 @@
+echo Auto build project at tomcat...
+set CATALINA_HOME=E:\dev\apache-tomcat-8.5.23
+set PROJ_HOME=D:\workspace\2017\springmvc
+
+if exist %CATALINA_HOME%\webapps\mvc121.war del %CATALINA_HOME%\webapps\mvc121.war /q
+echo Copy WAR to webapps...
+copy %PROJ_HOME%\target\mvc121.war %CATALINA_HOME%\webapps\mvc121.war
+
+@echo off&set local enable delayed expansion...  
+title Tomcat restart...
+cd /D %CATALINA_HOME%\bin  
+call %CATALINA_HOME%\bin\shutdown.bat  
+ping -n 6 127.1 >nul  
+wmic process where name="java.exe" get processid,commandline |findstr /i "tomcat" >#  
+for /f "delims=*" %%i in (#) do (        
+   set var=%%i  
+   set var=!var:start= #!  
+   for /f "tokens=3 delims=#" %%a in ("!var!") do (set tomcatpid=%%a)  
+)  
+del # >nul  
+if defined tomcatpid taskkill /pid !tomcatpid!  
+ping -n 3 127.1 >nul  
+call %CATALINA_HOME%\bin\startup.bat  
